59f9168a4f74100011047f6eab509f9969e4c48b
Raynaud Cornille <59450899+Raynaud123@users.noreply.github.com>
2023-03-28 18:19:22 +0000

    mongoose connection

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose=require('mongoose')
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,6 +12,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+//create new user
+app.post('/api/users', function (req,res,next){
+    req.username = req.body['username'];
+    next();
+},(req,res,next)=>{
+    
+})
 
 
 
