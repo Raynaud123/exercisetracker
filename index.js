9cd26fd2cf50b2d7e1e41d4519a7fee4b3f501e7
Raynaud Cornille <59450899+Raynaud123@users.noreply.github.com>
2023-03-28 18:46:08 +0000

    Adding user to MongoDB

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const cors = require('cors')
 require('dotenv').config()
 const mongoose=require('mongoose')
 mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//Mongoose Code
+const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+  username:String
+})
+
+const exerciseSchema = new Schema({
+  username:String,
+  description:String,
+  duration:Number,
+  date:Date,
+})
+
+const user = mongoose.model("UserModel", userSchema);
+const exercise = mongoose.model("exerciseModel", exerciseSchema);
+
+//Express
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -13,11 +32,17 @@ app.get('/', (req, res) => {
 });
 
 //create new user
-app.post('/api/users', function (req,res,next){
+app.post('/api/users', function(req,res,next){
     req.username = req.body['username'];
     next();
 },(req,res,next)=>{
-    
+    const userVar = new user({
+      username: req.username
+    })
+    userVar.save(function(err,data){
+        if(err) return console.error(err);    
+  res.json({"username":data["username"],"_id":data["_id"]})
+    })
 })
 
 
